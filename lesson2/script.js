370065acc5b59872ff1c6ae2d4b0cb7e5f7475fe
Sergey <repin.sergej@gmail.com>
2019-04-20 12:12:07 +0300

    Вторая работа

---
+++ lesson2/script.js
@@ -0,0 +1,47 @@
+var money = +prompt("Ваш бюджет на месяц?", '');
+var time = prompt("Введите дату в формате YYYY-MM-DD");
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
+for (let i =0; i<2; i ++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+    let b = prompt("Во сколько обойдется?", '');
+    if( (typeof(a)==='string') && (typeof(a)!== null) && (typeof(b)!== null)
+        && a !=='' && b !== '' && a.length <50) {
+            console.log("Done");
+            appData.expenses[a] = b;
+        } else {
+            i--;
+        }
+}
+/* 
+let j = 0;
+while (j<2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+    let b = prompt("Во сколько обойдется?", '');
+}
+
+
+let k =0;
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+    let b = prompt("Во сколько обойдется?", '');
+}while(k<2);
+*/
+
+    
+appData.moneyPerDay = appData.budget / 30;
+if (appData.moneyPerDay <100) {
+    console.log("Минимальный уровень достатка");
+}else if (appData.moneyPerDay >100 && appData.moneyPerDay < 2000){
+    console.log("Средний уровень достатка");
+}else if (appData.moneyPerDay >2000){
+    console.log("Высокий уровень достатка");
+}else {
+    console.log("Какая -то фигня");
+}
